d8cec008d50cf7899cd6d649e0dac23e119a5f6e
Keniel Peart <keniel_peart123@hotmail.com>
2023-02-08 13:18:33 +0000

    Update temp_conversion.py

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,10 @@
 # Todo: Code is a bit unclear
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
+def convert_fahr_to_celcius(fahr):
+    celcius = (fahr - 32) * (5 / 9)
+    return celcius
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+def convert_fahr_to_kelvin(fahr):
+    celcius = convert_fahr_to_celcius(fahr)
+    kelvin = celcius + 273.15
+    return kelvin
